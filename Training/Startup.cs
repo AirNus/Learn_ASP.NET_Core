a9e9f4d83d566d55656204d69fe66f83f64c3293
Ainur Mazitov <58343285+AirNus@users.noreply.github.com>
2020-08-31 15:51:00 +0300

    First day

--- Training/Startup.cs
+++ Training/Startup.cs
@@ -22,13 +22,14 @@ namespace Training
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            
             app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseToken("token");
 
             app.UseMiddleware<RoutingMiddleware>();
-            
+
+            app.UseFileServer();
         }
     }
 }
