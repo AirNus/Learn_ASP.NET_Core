5e70e0a5844f0022eab9d1ea53604d9caea26ff5
Ainur Mazitov <58343285+AirNus@users.noreply.github.com>
2020-09-15 09:35:11 +0300

    Lesson 4

---
+++ Training/ConfigurationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    public class ConfigurationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public IConfiguration AppConfiguration { get; set; }
+        
+        public ConfigurationMiddleware(RequestDelegate next,IConfiguration config)
+        {
+            this._next = next;
+            this.AppConfiguration = config;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await context.Response.WriteAsync($"<h1><font color=\"{AppConfiguration["color:header"]}\">{AppConfiguration["text:header"]}</font></h1><br/><p><font color=\"{AppConfiguration["color:footer"]}\">{AppConfiguration["text:footer"]}</font></p>");
+
+        }
+    }
+}
--- Training/Startup.cs
+++ Training/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,9 +15,20 @@ namespace Training
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940  
+        public Startup(IConfiguration config)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("TrainyConf.json")
+                .AddConfiguration(config);
+
+            AppConfiguration = builder.Build();
+        }
+        public IConfiguration AppConfiguration { get; set; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Добавляем конфигурацию в контейнер для DI
+            services.AddTransient<IConfiguration>(provider => AppConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,7 +40,7 @@ namespace Training
             //app.UseToken("token");
 
             // Меняем статус проекта на Продакш
-            env.EnvironmentName = "Production";
+            // env.EnvironmentName = "Production";
 
             // Переадрисация на https адреса в случае обращение по http
             app.UseHttpsRedirection();
@@ -46,7 +58,19 @@ namespace Training
 
                 app.UseExceptionHandler("/error");
             }
-
+            
+            // Отправили конфигурацию в контейнер и используем через DependencyInjection
+            //app.UseMiddleware<ConfigurationMiddleware>();
+           
+            // Считываем данные-конфигурацию с JSON файла
+           // var headerColor = AppConfiguration["color:header"];
+           // var footerColor = AppConfiguration["color:footer"];
+           // var header = AppConfiguration["text:header"];
+           // var footer = AppConfiguration["text:footer"];
+           // app.Run( async (context) =>
+           //{
+           //    await context.Response.WriteAsync($"<h1><font color=\"{headerColor}\">{header}</font></h1><br/><p><font color=\"{footerColor}\">{footer}</font></p>");
+           //});
 
             // Обработка ошибок в HTTP
             app.UseStatusCodePages();
@@ -55,7 +79,7 @@ namespace Training
 
 
             // Слой в котором указаны пути и их контент ( класс RoutingMiddleware )
-            app.UseMiddleware<RoutingMiddleware>();
+            // app.UseMiddleware<RoutingMiddleware>();
 
             // Метод который ищет файлы с названием index\default  и выставляет их как корневые при запуске страницы. Можно поменять
             app.UseFileServer();
